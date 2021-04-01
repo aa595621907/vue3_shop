45cba8219d8eb7899c5e74a75a61afbbb6341a27
乌龟不会爬行 <aa595621907@163.com>
2021-04-01 16:49:50 +0800

    初始化

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+    Login
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Login'
+})
+</script>
+
+<style lang="less" scoped></style>
